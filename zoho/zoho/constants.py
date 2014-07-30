794fc56f582f205f7492a54549903ecebe397b06
Jared Patrick <jared@bitwurx.com>
2014-07-29 22:29:53 -0400

    Update constants.py

--- zoho/zoho/constants.py
+++ zoho/zoho/constants.py
@@ -11,7 +11,7 @@ import getpass
 
 # zoho api
 ZOHO_API_USER = "jared.p@heritageps.net"
-ZOHO_API_PASS = "Summoner1!" # getpass.getpass("ZOHO password: ")
+ZOHO_API_PASS = "" # getpass.getpass("ZOHO password: ")
 ZOHO_API_AUTH_URI = \
 	"https://accounts.zoho.com/apiauthtoken/nb/create?SCOPE=ZohoCRM/crmap"
 ZOHO_CRM_API_URI = "https://crm.zoho.com/crm/private/xml"
@@ -21,4 +21,4 @@ ZOHO_CRM_SCOPE = "crmapi"
 # database
 DB_NAME = "crm"
 DB_USER = "jared"
-DB_PASS = "9tailz" # getpass.getpass("Postgres password: ")
+DB_PASS = "" # getpass.getpass("Postgres password: ")
